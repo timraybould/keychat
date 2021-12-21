eda9fa06894ec5e597e83cc1e19bdb7e8979a472
Tim Raybould <timraybould@me.com>
2021-12-20 20:22:17 -0500

    test json serve

--- src/App.tsx
+++ src/App.tsx
@@ -17,10 +17,28 @@ const enum Connectors {
   WALLET_CONNECT = 'walletconnect',
 }
 
+// const replayAttack = new SiweMessage({
+//   domain: "localhost:3000",
+//   address: "0x85245E81C59aA83c43992B0162bC5B9f5c636F25",
+//   chainId: "1",
+//   uri: "http://localhost:3000",
+//   version: "1",
+//   statement: "Some text that you will recognize",
+//   nonce: "O3NpjFMmZiyG1UnVK",
+//   issuedAt: "2021-12-19T00:31:41.322Z",
+//   signature: "0x7748200a3dd0cac1bccdd2838abf8fadaca5fb5dac59e93842bf72aae2236e9b12bb2f0393c4a7b47c00d42e5a427a604bad052eb549fa172734e35e169d66fc1c"
+// })
+
 export default function App() {
   const [awaitingVerification, setAwaitingVerification] = useState(false)
   const [ethAddress, setEthAddress] = useState("")
   const [ensString, setEnsString] = useState("")
+
+  // async function Replay () {
+  //   const validation = await replayAttack.validate()
+  //   console.log(validation)
+  //   setEthAddress(replayAttack.address)
+  // }
 
   async function generateSignValidateMessage(provider: ethers.providers.Web3Provider) {
     try {
@@ -30,6 +48,8 @@ export default function App() {
       }
       const ens = await provider.lookupAddress(address) || ""
       const nonce = generateNonce()
+      const expiration = new Date(Date.now() + 5*60000)
+      const notBefore = new Date()
       const message = new SiweMessage({
           domain: document.location.host,
           address,
@@ -38,12 +58,16 @@ export default function App() {
           version: '1',
           statement: 'Some text that you will recognize',
           type: SignatureType.PERSONAL_SIGNATURE,
+          expirationTime: expiration.toISOString(),
+          notBefore: notBefore.toISOString(),
           nonce,
       })
       const signature = await provider.getSigner().signMessage(message.signMessage())
       message.signature = signature
+      console.log('signed', message)
       // need to pass a JsonRpcProvider to validate if https://eips.ethereum.org/EIPS/eip-1271 is needed
-      await message.validate()
+      const validation = await message.validate()
+      console.log('validation', validation)
       setEthAddress(address)
       setEnsString(ens)
     } catch (error) {
@@ -84,16 +108,16 @@ export default function App() {
                   ?
                     <div>
                       <h2>Give any Ethereum address a voice</h2>
-                      <button onClick={() => signIn(Connectors.WINDOW)}>
+                      <button className='button' onClick={() => signIn(Connectors.WINDOW)}>
                         <EthereumLogo />
                         <span>Sign in with Ethereum</span>
                       </button>
-                      <a onClick={() => signIn(Connectors.WALLET_CONNECT)}>Use a wallet outside of the browser?</a>
+                      <button className='linkButton' onClick={() => signIn(Connectors.WALLET_CONNECT)}>Use a wallet outside of the browser?</button>
                     </div>
                   :
                     <div>
                       <h2>Give any Ethereum address a voice</h2>
-                      <button onClick={() => signIn(Connectors.WALLET_CONNECT)}>
+                      <button className='button' onClick={() => signIn(Connectors.WALLET_CONNECT)}>
                         <EthereumLogo />
                         <span>Sign in with Ethereum</span>
                       </button>
@@ -102,13 +126,13 @@ export default function App() {
                 <div>
                   <h2>You control the following keychat IDs</h2>
                   <div className="keychatIdResults">
-                    <span>{ethAddress}@keychat.xyz</span>
-                    <a>Set or reset the password</a>
+                    <span>@{ethAddress}:keychat.xyz</span>
+                    <button className='linkButton'>Set or reset the password</button>
                   </div>
                   {ensString !== "" &&
                     <div className="keychatIdResults">
-                      <span>{ensString}@keychat.xyz</span>
-                      <a>Set or reset the password</a>
+                      <span>@{ensString}:keychat.xyz</span>
+                      <button className='linkButton'>Set or reset the password</button>
                     </div>
                   }
                 </div>
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+if (window.location.pathname === "/.well-known/matrix/server") {
+  window.location.replace(`https://${window.location.host}/.well-known/matrix/server.json`);
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
